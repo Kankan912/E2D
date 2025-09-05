667ed994ea2f25699a82ad0149048561eb8e4810
Kankan912 <kankanway912@gmail.com>
2025-09-05 11:57:13 +0100

    Updated DashboardCard.tsx

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ReactNode } from 'react'
-import { LucideIcon } from 'lucide-react'
+import { DivideIcon as LucideIcon } from 'lucide-react'
 
 interface DashboardCardProps {
   title: string
